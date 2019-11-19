485daafc4a9fc35cfc7156af42f928be4bdfef49
libreazer <52778262+libreazer@users.noreply.github.com>
2019-11-19 14:47:10 +0200

    Update compress-button.sh

--- compress-button.sh
+++ compress-button.sh
@@ -9,8 +9,12 @@ echo ${file##*/} > .filename.txt #filename
 filename=$(cat .filename.txt) #file
 finalname=${filename%.pdf} #without
 output=$(echo ${finalname##*/}) #extension
+dir=$(dirname "${file}")
 #Output Backend
 
+#File Size
+#Under Construction
+#File Size
 
 #Compress File
 gs -q -dNOPAUSE -dBATCH -dSAFER -dPDFA=2 -dPDFACompatibilityPolicy=1 \
@@ -21,12 +25,23 @@ gs -q -dNOPAUSE -dBATCH -dSAFER -dPDFA=2 -dPDFACompatibilityPolicy=1 \
 -dGrayImageResolution=150 -dMonoImageDownsampleType=/Bicubic \
 -dMonoImageResolution=150 -dColorConversionStrategy=/Gray \
 -dProcessColorModel=/DeviceGray \
--sOutputFile=$output-compressed.pdf $file
+-sOutputFile=$dir/$output-compressed.pdf $file
 #Compress File
 
+#Success Message
+if [ -e $dir/$output-compressed.pdf ]
+then
+  python3 success.py
+fi
+if [ -s $dir/$output-compressed.pdf ]
+then
+  python3 error.py
+fi
+#Success Message
 
 #Remove Temp
 rm .filename.txt
+rm input.txt
 #Remove Temp
 
 #Compress Button
