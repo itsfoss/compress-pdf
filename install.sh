78d356a661fc943cfbfb46c8808d5ae294dcc46e
Debdut Chakraborty <andanotheremailid@gmail.com>
2020-06-10 10:14:35 +0530

    better install process (removed data_files, modified install script)

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ pip3 install -r requirements.txt && \
     python3 setup.py bdist_wheel && \
         pip3 install dist/*.whl
 
+sudo mkdir -p /usr/share/itsfoss/resources/compress-pdf
+sudo cp resources/*.png /usr/share/itsfoss/resources/compress-pdf/
+
 if [[ -d $HOME/.local/share/applications ]]; then
     cp compresspdf.desktop $HOME/.local/share/applications/
 else
